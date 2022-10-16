476983168973de5640c702b38888515171bd173e
Vitaly Posadnev <elemabor@gmail.com>
2022-10-16 18:35:39 +0300

    feat(main): update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 if __name__ == '__main__':
     with open('./data.json', 'r') as f:
@@ -11,20 +13,19 @@ if __name__ == '__main__':
 
     options = uc.ChromeOptions()
     prefs = {
-        "download.default_directory": "./uploads",
+        "download.default_directory": "/home/vitaly/PycharmProjects/google-codejam-downloader/uploads/",
         "profile.default_content_settings.popups": 0,
     }
     options.add_experimental_option("prefs", prefs)
     driver = uc.Chrome(debug=True, options=options)
     driver.get('chrome://settings/')
-    driver.execute_script('chrome.settingsPrivate.setDefaultZoom(0.01);')
+    driver.execute_script('chrome.settingsPrivate.setDefaultZoom(0.25);')
 
     prefix = ''
     result = []
-    it1 = 0
-    for d in data:
-        it1 += 1
-        print(f'Парсинг {it1}/{len(data)} соревнования: {it1/len(data)}%')
+    flag = False
+    for it1, d in enumerate(data):
+        print(f'Парсинг {it1 + 1}/{len(data)} соревнования: {(it1 + 1) / len(data)}%')
         if 'Code Jam' in d['adventure_name'] and 'Code Jam to' not in d['adventure_name']:
             prefix = 'codejam'
         elif 'Hash Code' in d['adventure_name']:
@@ -36,42 +37,58 @@ if __name__ == '__main__':
         else:
             continue
 
-        challenge_id = d['challenge_id']
-        challenge_name = d['challenge_name']
+        if str(d['adventure_name']).count('2018') != 0:
+            flag = True
+
+        if not flag:
+            continue
 
         adventure_id = d['adventure_id']
         adventure_name = d['adventure_name']
-        it2 = 0
-        for user_score in d['user_scores']:
-            it2 += 1
-            print(f"Парсинг результатов {it2}/{len(d['user_scores'])}: {it2/len(d['user_scores'])}%")
-            user_name = user_score['competitor']['displayname']
-            user_id = user_score['competitor']['id']
 
-            url = f'https://codingcompetitions.withgoogle.com/{prefix}/submissions/{challenge_id}/{user_id}'
-            driver.get(url)
-            time.sleep(20)
-            buttons = driver.find_elements(By.CLASS_NAME, "download-button")
-            for button in buttons:
+        for it2, challenge in enumerate(d['challenges']):
+            challenge_id = challenge['challenge']['id']
+            challenge_name = challenge['challenge']['title']
+            print(f"Парсинг результатов этапа {it2 + 1}/{len(d['challenges'])}: {(it2 + 1) / len(d['challenges'])}%")
+
+            for it3, user_score in enumerate(challenge['user_scores']):
+                print(
+                    f"Парсинг результатов {it3 + 1}/{len(challenge['user_scores'])}: {(it3 + 1) / len(challenge['user_scores'])}%")
+                user_id = user_score['competitor']['id']
+                user_name = user_score['competitor']['displayname']
+
+                url = f'https://codingcompetitions.withgoogle.com/{prefix}/submissions/{challenge_id}/{user_id}'
+                driver.get(url)
                 try:
-                    before = os.listdir('./uploads')
-                    button.click()
-                    time.sleep(5)
-                    after = os.listdir('./uploads')
-                    change = set(after) - set(before)
-                    if len(change) == 1:
-                        file_name = change.pop()
-                        request = {
-                            "file_name": file_name,
-                            "user_id": user_id,
-                            "user_name": user_name,
-                            "challenge_id": challenge_id,
-                            "challenge_name": challenge_name,
-                            "adventure_id": adventure_id,
-                            "adventure_name": adventure_name
-                        }
-                        result.append(request)
-                except:
+                    wait = WebDriverWait(driver, 20)
+                    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ranking-table__row--header")))
+                except Exception as e:
+                    print("Не дождались загрузки...")
                     continue
-    with open('data.json', 'w', encoding='utf-8') as file:
+                buttons = driver.find_elements(By.CLASS_NAME, "download-button")
+                if len(buttons) == 0:
+                    break
+                for button in buttons:
+                    try:
+                        before = os.listdir('/home/vitaly/Downloads')
+                        button.click()
+                        time.sleep(4)
+                        after = os.listdir('/home/vitaly/Downloads')
+                        change = set(after) - set(before)
+                        if len(change) == 1:
+                            file_name = change.pop()
+                            request = {
+                                "file_name": file_name,
+                                "user_id": user_id,
+                                "user_name": user_name,
+                                "challenge_id": challenge_id,
+                                "challenge_name": challenge_name,
+                                "adventure_id": adventure_id,
+                                "adventure_name": adventure_name
+                            }
+                            result.append(request)
+                            print(request)
+                    except Exception as e:
+                        print("Ошибка!!!")
+    with open('result.json', 'w', encoding='utf-8') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
